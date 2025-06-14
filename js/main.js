2a238bd72b53f67d0402f5611acb3158516a623c
Rahul <codonoid@gmail.com>
2025-06-14 16:41:22 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,82 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+    
+})(jQuery);
+
+// gallery
+
+const viewMoreBtn = document.getElementById('viewMoreBtn');
+const hiddenImages = document.querySelectorAll('.custom-gallery-hidden');
+
+viewMoreBtn.addEventListener('click', () => {
+  let anyVisible = false;
+
+  hiddenImages.forEach(img => {
+    const isHidden = window.getComputedStyle(img).display === 'none';
+    img.style.display = isHidden ? 'block' : 'none';
+    if (!isHidden) anyVisible = true;
+  });
+
+  viewMoreBtn.textContent = anyVisible ? 'View More' : 'Show Less';
+});
